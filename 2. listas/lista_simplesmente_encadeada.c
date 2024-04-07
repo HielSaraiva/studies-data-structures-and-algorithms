512f269eb6918aeaa24c932bc931a73e0eff7abb
Hiel Saraiva Freitas de Queiroga <hielsaraiva11.hs@gmail.com>
2024-04-07 11:47:40 -0300

    fixing and adding some methods

--- 2. listas/lista_simplesmente_encadeada.c
+++ 2. listas/lista_simplesmente_encadeada.c
@@ -17,11 +17,14 @@ struct no
 };
 
 // Prototipos de funcoes:
-void listar(const struct no *lista);
-struct no *buscar(const struct no *lista, char matricula[]);
-// void alterar(struct no *lista, int valor, int novo_valor);
+void listar(struct no *lista);
+struct no *buscar(struct no *lista, char matricula[]);
+void alterar(struct no *lista, char matricula[], struct s_dados dados);
 void inserir_final(struct no **lista, struct s_dados dados);
 void inserir_comeco(struct no **lista, struct s_dados dados);
+int remover(struct no **lista, char matricula[]);
+int esvaziar(struct no **lista);
+struct no *pega_no_anterior(struct no *lista, struct no *no);
 
 int main()
 {
@@ -36,15 +39,50 @@ int main()
 	dados1.notas[0] = 7.8f;
     dados1.notas[1] = 9.6f;
 
+    // Iniciando dados2:
+    struct s_dados dados2;
+    strcpy(dados2.nome, "Geni Saraiva");
+    strcpy(dados2.matricula, "202302");
+    strcpy(dados2.fone, "85988441830");
+    dados2.notas[0] = 10.0f;
+    dados2.notas[1] = 8.5f;
+
+    // Iniciando dados3:
+    struct s_dados dados3;
+    strcpy(dados3.nome, "Rodrigo");
+    strcpy(dados3.matricula, "202303");
+    strcpy(dados3.fone, "85988442342");
+    dados3.notas[0] = 1.0f;
+    dados3.notas[1] = 3.5f;
+
+    // Iniciando dados4:
+    struct s_dados dados4;
+    strcpy(dados4.nome, "Conrado");
+    strcpy(dados4.matricula, "202304");
+    strcpy(dados4.fone, "85988442233");
+    dados4.notas[0] = 5.0f;
+    dados4.notas[1] = 4.5f;
 
 	// Testando funcoes:
-    inserir_comeco(&lista, dados1);
-	listar(lista);
+    inserir_final(&lista, dados1);
+    inserir_final(&lista, dados2);
+    inserir_final(&lista, dados3);
+    //alterar(lista, "202301", dados4);
 
-	
+    //listar(lista);
+    //printf("\n%s\n", ((buscar(lista, "202301"))->dados).nome);
+
+    //remover(&lista, "202303");
+    esvaziar(&lista);
+    listar(lista);
+
+    inserir_final(&lista, dados1);
+    inserir_final(&lista, dados2);
+    inserir_final(&lista, dados3);
+    listar(lista);
 }
 
-// Inserir no comeco:
+// Inserir no comeco(C):
 void inserir_comeco(struct no **lista, struct s_dados dados)
 {
 	if (*lista == NULL)
@@ -65,7 +103,7 @@ void inserir_comeco(struct no **lista, struct s_dados dados)
 	}
 }
 
-// Inserir no final:
+// Inserir no final(C):
 void inserir_final(struct no **lista, struct s_dados dados)
 {
 	if (*lista == NULL)
@@ -79,51 +117,100 @@ void inserir_final(struct no **lista, struct s_dados dados)
 	else
 	{
 		// Se ja houver algum no:
-		struct no *aux = malloc(sizeof(struct no));
-		aux->dados = dados;
-		aux->prox = NULL;
-		(*lista)->prox = aux;
+		struct no *aux1 = malloc(sizeof(struct no));
+		aux1->dados = dados;
+		aux1->prox = NULL;
+
+        // aux2 percorre a lista até o último dado:
+        struct no *aux2 = *lista;
+        while(aux2->prox != NULL)
+        {
+            aux2 = aux2->prox;
+        }
+        (aux2->prox) = aux1;
 	}
 }
 
 // Inserir ordenando:
 
-// Remover:
+// Remover(C):
+int remover(struct no **lista, char matricula[])
+{
+    struct no *aux = buscar(*lista, matricula);
+    if(*lista == NULL || aux == NULL) {
+        return -1; // nao ha o que remover
+    }
 
-// Ordenar:
+    if(aux == *lista) { // esta no comeco
+        *lista = (*lista)->prox;
+        free(aux);
+    } else if(aux->prox == NULL) { // esta no fim
+        struct no *ant = pega_no_anterior(*lista, aux);
+        ant->prox = NULL;
+        free(aux);
+    } else { //esta no meio
+        struct no *ant = pega_no_anterior(*lista, aux);
+        ant->prox = aux->prox;
+        free(aux);
+    }
+    return 0; // dado removido da lista
+}
 
-// Alterar:
-//void alterar(struct no *lista, int valor, int novo_valor)
-//{
-//	struct no *no = buscar(lista, valor);
-//	if (no)
-//	{
-//		no->valor = novo_valor;
-//	}
-//}
+// Esvaziar lista(C):
+int esvaziar(struct no **lista) {
+    if(*lista == NULL) {
+        return -1; // a lista ja esta vazia
+    }
 
-// Esvaziar lista:
+    while(*lista != NULL) {
+        struct no *aux = *lista;
+        (*lista) = (*lista)->prox;
+        free(aux);
+    }
+    return 0; // lista esvaziada
+}
 
-// Buscar:
-struct no *buscar(const struct no *lista, char matricula[])
+// Alterar(C):
+void alterar(struct no *lista, char matricula[], struct s_dados dados)
+{
+    struct no *no = buscar(lista, matricula);
+    if (no)
+    {
+        no->dados = dados;
+    }
+}
+
+// Buscar(C):
+struct no *buscar(struct no *lista, char matricula[])
 {
 	struct no *aux = lista;
-	while (aux != NULL && aux->dados.matricula)
+	while (aux != NULL && strcmp((aux->dados).matricula, matricula) != 0)
 	{
 		aux = aux->prox;
 	}
+	return aux;
 }
 
-// Listar todos:
-void listar(const struct no *lista)
+// Listar todos(C):
+void listar(struct no *lista)
 {
 	struct no *aux = lista;
-	while (aux->prox != NULL)
+	while (aux != NULL)
 	{
 		printf("\n%s\n", aux->dados.nome);
 		printf("%s\n", aux->dados.matricula);
 		printf("%s\n", aux->dados.fone);
-		printf("%f %f\n", aux->dados.notas[0], aux->dados.notas[1]);
+		printf("%.2f %.2f\n", aux->dados.notas[0], aux->dados.notas[1]);
 		aux = aux->prox;
 	}
+}
+
+// Funcoes auxiliares(C):
+struct no *pega_no_anterior(struct no *lista, struct no *no)
+{
+    struct no *aux = lista;
+    while(aux != NULL && aux->prox != no) {
+        aux = aux->prox;
+    }
+    return aux;
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Estrutura de Dados:
 Repositório que contém as implementações das operações necessárias para as seguintes estruturas de dados.
 
-* Lista simplesmente encadeada
+* Lista simplesmente encadeada (ordenada e não ordenada)
 
 * Lista duplamente encadeada
